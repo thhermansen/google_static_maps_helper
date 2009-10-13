913f2044b9cf79616f397da5b6c163d355f548b9
Thorbjørn Hermansen <thhermansen@gmail.com>
2009-10-13 15:19:16 +0200

    We can now push markers on map.

--- lib/google_static_maps_helper/map.rb
+++ lib/google_static_maps_helper/map.rb
@@ -3,17 +3,37 @@ module GoogleStaticMapsHelper
   # It holds markers and iterates over them to build the URL
   # to be used in an image tag.
   class Map
+    include Enumerable
+
     # Raised when required options is not sent in during construction
     class OptionMissing < ArgumentError; end
     
     REQUIRED_OPTIONS = [:key, :size, :sensor]
-
-
+    OPTIONAL_OPTIONS = [:center, :zoom, :size, :format, :maptype, :mobile, :language]
+    
     def initialize(options)
       validate_required_options(options)
+
+      @markers = []
+    end
+    
+    def <<(marker)
+      @markers << marker
+      @markers.uniq!
     end
 
+    def each
+      @markers.each {|m| yield(m)}
+    end
 
+    def empty?
+      @markers.empty?
+    end
+
+    def length
+      @markers.length
+    end
+    
     private
     def validate_required_options(options)
       missing_options = REQUIRED_OPTIONS - options.keys
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -7,6 +7,7 @@ describe GoogleStaticMapsHelper::Map do
     :sensor => false
   }
 
+
   describe "initialize" do
     @@require_options.each_key do |key|
       it "should raise OptionMissing if #{key} is not given" do
@@ -14,6 +15,27 @@ describe GoogleStaticMapsHelper::Map do
         option_with_missing_option.delete(key)
         lambda {GoogleStaticMapsHelper::Map.new(option_with_missing_option)}.should raise_error(GoogleStaticMapsHelper::Map::OptionMissing)
       end
+    end
+  end
+
+  describe "markers" do
+    before :each do
+      @marker = GoogleStaticMapsHelper::Marker.new(:lat => 1, :lng => 2)
+      @map = GoogleStaticMapsHelper::Map.new(@@require_options)
+    end
+
+    it "should be empty as default" do
+      @map.should be_empty
+    end
+
+    it "should be able to push markers onto map" do
+      @map << @marker
+    end
+
+    it "should not be possible to push the same marker twice" do
+      @map << @marker
+      @map << @marker
+      @map.length.should == 1
     end
   end
 end
