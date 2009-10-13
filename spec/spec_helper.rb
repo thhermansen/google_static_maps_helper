d525c99e61b712f24cd469c52f2da3cf62d13a94
Thorbjørn Hermansen <thhermansen@gmail.com>
2009-10-13 12:41:23 +0200

    Initial commit to google_static_maps_helper.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'google_static_maps_helper'
+require 'spec'
+require 'spec/autorun'
+
+Spec::Runner.configure do |config|
+  
+end
