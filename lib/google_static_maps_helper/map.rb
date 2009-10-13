8375cee2d148eac84bafa7260f57783d219349bf
Thorbjørn Hermansen <thhermansen@gmail.com>
2009-10-13 15:00:07 +0200

    Map does now require some options during construction.
    These keys does the Google Static Maps API need too..

--- lib/google_static_maps_helper/map.rb
+++ lib/google_static_maps_helper/map.rb
@@ -3,6 +3,21 @@ module GoogleStaticMapsHelper
   # It holds markers and iterates over them to build the URL
   # to be used in an image tag.
   class Map
+    # Raised when required options is not sent in during construction
+    class OptionMissing < ArgumentError; end
     
+    REQUIRED_OPTIONS = [:key, :size, :sensor]
+
+
+    def initialize(options)
+      validate_required_options(options)
+    end
+
+
+    private
+    def validate_required_options(options)
+      missing_options = REQUIRED_OPTIONS - options.keys
+      raise OptionMissing, "The following required options are missing: #{missing_options.join(', ')}" unless missing_options.empty?
+    end
   end
 end
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -1,7 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe GoogleStaticMapsHelper::Map do
-  describe "initialize" do
+  @@require_options = {
+    :size => '800x600',
+    :key => 'MY_GOOGLE_KEY',
+    :sensor => false
+  }
 
+  describe "initialize" do
+    @@require_options.each_key do |key|
+      it "should raise OptionMissing if #{key} is not given" do
+        option_with_missing_option = @@require_options.dup
+        option_with_missing_option.delete(key)
+        lambda {GoogleStaticMapsHelper::Map.new(option_with_missing_option)}.should raise_error(GoogleStaticMapsHelper::Map::OptionMissing)
+      end
+    end
   end
 end
