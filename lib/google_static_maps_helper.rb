274a7e912a21ac99815618bbebf91e48ce3d5359
Thorbjørn Hermansen <thhermansen@gmail.com>
2009-10-13 14:38:01 +0200

    Started the work on Map class, and wrote a bit in the README file.

--- lib/google_static_maps_helper.rb
+++ lib/google_static_maps_helper.rb
@@ -1,4 +1,5 @@
 # coding:utf-8  
+require File.dirname(__FILE__) + '/google_static_maps_helper/map'
 require File.dirname(__FILE__) + '/google_static_maps_helper/marker'
 
 module GoogleStaticMapHelper
---
+++ lib/google_static_maps_helper/map.rb
@@ -0,0 +1,8 @@
+module GoogleStaticMapsHelper
+  # Represents the map we are generating
+  # It holds markers and iterates over them to build the URL
+  # to be used in an image tag.
+  class Map
+    
+  end
+end
