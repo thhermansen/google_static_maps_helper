4e0a5628348828e1bdf811f56315265b7410b3b9
Thorbjørn Hermansen <thhermansen@gmail.com>
2009-10-13 15:29:09 +0200

    Added the API url.

--- lib/google_static_maps_helper.rb
+++ lib/google_static_maps_helper.rb
@@ -3,4 +3,5 @@ require File.dirname(__FILE__) + '/google_static_maps_helper/map'
 require File.dirname(__FILE__) + '/google_static_maps_helper/marker'
 
 module GoogleStaticMapHelper
+  API_URL = 'http://maps.google.com/maps/api/staticmap'
 end
