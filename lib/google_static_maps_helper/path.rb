a503b01e9f37879540bcd2624ebb45c39b0551a2
Thorbjørn Hermansen <thhermansen@gmail.com>
2009-10-29 11:50:01 +0100

    Wrote better doc for Path.

--- lib/google_static_maps_helper/path.rb
+++ lib/google_static_maps_helper/path.rb
@@ -1,4 +1,8 @@
 module GoogleStaticMapsHelper
+  #
+  # A Path is used to draw things in the map, either lines or Polygons.
+  # It is build up of points and if a fill color is set you'll get a Polygon.
+  #
   class Path
     include Enumerable
 
@@ -6,13 +10,31 @@ module GoogleStaticMapsHelper
 
     attr_accessor :points, *OPTIONAL_OPTIONS
 
+    #
+    # Creates a new Path which you can push points on to to make up lines or polygons
+    #
+    # The following options are available, for more information see the 
+    # Google API documentation[http://code.google.com/apis/maps/documentation/staticmaps/#Paths].
+    #
+    # <tt>:weight</tt>::    The weight is the thickness of the line, defaults to 5
+    # <tt>:color</tt>::     The color of the border can either be a textual representation like red, green, blue, black etc
+    #                       or as a 24-bit (0xAABBCC) or 32-bit hex value (0xAABBCCDD). When 32-bit values are
+    #                       given the two last bits will represent the alpha transparency value.
+    # <tt>:fillcolor</tt>:: With the fill color set you'll get a polygon in the map. The color value can be the same
+    #                       as described in the <tt>:color</tt>. When used, the static map will automatically create
+    #                       a closed shape.
+    # <tt>:points</tt>::    An array of points. You can mix objects responding to lng and lat, and a Hash with lng and lat keys.
+    #
     def initialize(options = {})
       @points = []
       options.each_pair {|k, v| send("#{k}=", v)}
     end
 
-    
-    def url_params
+    #
+    # Returns a string representation of this Path
+    # Used by the Map when building the URL
+    #
+    def url_params # :nodoc:
       raise BuildDataMissing, "Need at least 2 points to create a path!" unless can_build?
       out = 'path='
      
@@ -30,6 +52,11 @@ module GoogleStaticMapsHelper
     end
 
   
+    #
+    # Sets the points of this Path.
+    # 
+    # *Warning:* Using this method will clear out any points which might be set.
+    #
     def points=(array)
       raise ArgumentError unless array.is_a? Array
       @points = []
@@ -48,18 +75,33 @@ module GoogleStaticMapsHelper
       length == 0
     end
 
+    #
+    # Pushes a new point into the Path
+    #
+    # A point might be a Hash which has lng and lat as keys, or an object responding to
+    # lng and lat. Any points pushed in will be converted internally to a Location
+    # object.
+    #
     def <<(point)
       @points << ensure_point_is_location_object(point)
       @points.uniq!
       self
     end
 
+
     private
+    #
+    # Extracts the lng and lat values from incomming point and creates a new Location
+    # object from it.
+    #
     def ensure_point_is_location_object(point)
       return point if point.instance_of? Location
       Location.new(point)
     end
-
+    
+    # 
+    # Do we have enough points to build a path?
+    #
     def can_build?
       length > 1
     end
